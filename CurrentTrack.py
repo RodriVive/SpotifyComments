aa66ca61eda6d16590ab3435d72f0de19d276144
Rodrigo Villegas <rodrigo.villegas.venegas@gmail.com>
2022-01-29 20:12:30 -0500

    Token will refresh

--- CurrentTrack.py
+++ CurrentTrack.py
@@ -2,6 +2,9 @@ from types import NoneType
 import spotipy
 import spotipy.util as util
 from dotenv import dotenv_values
+import tekore as tk
+from spotipy.oauth2 import SpotifyOAuth
+import json
 
 
 def main(username):
@@ -9,13 +12,16 @@ def main(username):
     env = dotenv_values('.env')
     # Get a token to acces user info
     scope = 'user-read-currently-playing user-read-recently-played user-read-playback-state'
-    token = util.prompt_for_user_token(username, scope, client_id=env['client_id'], client_secret=env['client_secret'], redirect_uri='http://127.0.0.1:9090')
+    #token = util.prompt_for_user_token(username, scope, client_id=env['client_id'], client_secret=env['client_secret'], redirect_uri='http://127.0.0.1:9090')
+    token = tk.prompt_for_user_token(client_id=env['client_id'], client_secret=env['client_secret'], redirect_uri='http://127.0.0.1:9090', scope=scope)
+    #oauth = spotipy.oauth2.SpotifyOAuth(client_id=env['client_id'], client_secret=env['client_secret'], redirect_uri='http://127.0.0.1:9090', scope=scope,  username=username)
+
     currentSong(token, username)
 
 def currentSong(token, username):
     if token:
         # Creates new spotify object with the given token to access user info
-        sp = spotipy.Spotify(auth=token)
+        sp = spotipy.Spotify(auth=token, requests_timeout=10, retries=10)
         # Get username
         user = sp.current_user()
         print(user['id'])
@@ -37,11 +43,23 @@ def currentSong(token, username):
         print(song['item']['name'], song['item']['uri'])
         
         # Check if current song changes
-        while(True):
-            check = sp.current_user_playing_track()
-            if song['item']['name'] != check['item']['name']:
-                song = check
-                print(song['item']['name'], song['item']['uri'])
+        cache = open('.cache-' + username, 'r')
+        fileInfo = cache.readlines()
+        dict = json.loads(fileInfo[0])
+        
+        try:
+            while(True):
+                check = sp.current_user_playing_track()
+                if song['item']['name'] != check['item']['name']:
+                    song = check
+                    print(song['item']['name'], song['item']['uri'])
+        except:
+            tk.refresh_user_token(client_id=env['client_id'], client_secret=env['client_secret'], refresh_token=dict['refresh_token'])
+        finally:
+            currentSong(token, username)
+        
+    else:
+        print('TOKEN ERROR FOR', username)
             
 env = dotenv_values('.env')      
 main(env['username'])
